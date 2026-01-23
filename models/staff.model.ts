becbd61cf6ef2060e9351b5c9ee5d00f7c23d80d
Joerakesh <rakeshjoe52@gmail.com>
2026-01-23 09:21:41 +0530

    Required model created

---
+++ models/staff.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model, models } from "mongoose";
+
+const StaffSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String },
+    fatherName: { type: String },
+    dateOfBirth: { type: Date },
+    gender: { type: String },
+    contact: { type: String },
+    email: { type: String, required: true, unique: true },
+    role: {
+      type: String,
+      enum: ["PRINCIPAL", "STAFF"],
+      default: "STAFF",
+    },
+  },
+  { timestamps: true },
+);
+
+export default models.Staff || model("Staff", StaffSchema);
